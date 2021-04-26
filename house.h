5cdb3030c9779caef4cb06241089438cdd38de5f
Valen <valentinodidio943@gmail.com>
2021-04-25 21:13:34 -0300

    Update house.h

--- house.h
+++ house.h
@@ -15,7 +15,7 @@ unsigned char recibido, comando, command_data[10], command_data_index=0, command
 unsigned char ArrayRX[20], Indice, ArrayProc[20];
 
 //FUNCIONES
-void SendSerial(unsigned char *data, unsigned char lenght);
+void SendSerial(unsigned char *data);
 void RX_Byte(void);
 void ProcesarArray(void);
 unsigned char EsParaMi(void);
